8862c51700c7fe590385601e5c01fabe550ac6ca
qvandervelden <106928708+qvandervelden@users.noreply.github.com>
2022-06-08 22:03:17 +0200

    Add files via upload

--- EHCD.py
+++ EHCD.py
@@ -1,3 +1,21 @@
+"""
+EHCD: Efficient Heisenberg Chain Decomposition
+Copyright (C) 2022 Quinn van der Velden
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import numpy as np
 import sympy as sym
 import sympy.physics.wigner as wig
@@ -201,11 +219,6 @@ def block(spins, J, coupling, S):
                 runtime = time.time()
     return H_S
 
-#returns array of spin-values corresponding to a uniform set of spins
-def unispin(s, N):
-    return np.full((N),s)
-
-#returns coupling matrix for the Heisenberg Line of N particles with coupling J_s
 def uniline(N, J_s):
     J = np.zeros((N,N))
     for i in range(0,N):
